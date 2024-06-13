d3f621f7efcca59706d74edffb3cf25fdb5ce568
Imran Baig <ibaig5147@gmail.com>
2024-06-13 19:14:49 +0500

    practice of module Export in Nodejs

---
+++ data.js
@@ -0,0 +1,5 @@
+const arr = [1,2,3,4,5]
+let arr2 = ['A,B,C,D,E']
+
+
+module.exports = {arr, arr2}
---
+++ main.js
@@ -0,0 +1,14 @@
+const x = require('./data')
+
+
+
+
+
+console.log(x.arr);
+
+// let u = {
+//     abc : 10 ,
+//     y : 89
+// }
+
+// console.log(u.abc);
